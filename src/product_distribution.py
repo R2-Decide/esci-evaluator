832acd14d754c318a8987e13dbded9b968116ee3
Yogesh Patil <yp445@cornell.edu>
2024-12-26 21:20:48 -0500

    Refactor product_distribution.py to use context manager for file handling

--- src/product_distribution.py
+++ src/product_distribution.py
@@ -5,12 +5,13 @@ This script is used to calculate the distribution of products in the ESCI datase
 import json
 
 stats = {}
-for i in open("esci-s/en_esci.json", encoding="utf-8"):
-    if category := json.loads(i).get("category"):
-        top_category = category[0]
-        if top_category not in stats:
-            stats[top_category] = 0
-        stats[top_category] += 1
+with open("esci-s/esci.json", encoding="utf-8") as f:
+    for i in f:
+        if category := json.loads(i).get("category"):
+            top_category = category[0]
+            if top_category not in stats:
+                stats[top_category] = 0
+            stats[top_category] += 1
 
 stats = dict(sorted(stats.items(), reverse=True, key=lambda x: x[1]))
 
